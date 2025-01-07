94b722e9c0b1e1f399eb908a0211fc8ca458ebbe
Tejas Shah <shatejas@amazon.com>
2025-01-07 15:29:58 -0800

    Effecient filtering experiments

--- experiments/efficient-filtering/build.sh
+++ experiments/efficient-filtering/build.sh
@@ -2,35 +2,38 @@
 
 set -xe
 
-run_id=$1
-test_procedure=$2
-shift
+no_cache=$1
 shift
 service_names=$@
 
+
 EXPERIMENT_ROOT="experiments"
 EXPERIMENT_PATH="experiments/efficient-filtering"
+OSB_PARAMS_PATH="osb/custom/params"
+PARAMS_PATH="experiments/efficient-filtering/osb-params"
 ENV_PATH="${EXPERIMENT_PATH}/test.env"
 TMP_ENV_DIR="${EXPERIMENT_PATH}/tmp"
 TMP_ENV_NAME="test.env"
 TMP_ENV_PATH="${EXPERIMENT_PATH}/${TMP_ENV_NAME}"
-OSB_PARAMS_PATH="osb/custom/params"
-PARAMS_PATH="${EXPERIMENT_ROOT}/efficient-filtering/osb-params"
 
 source ${EXPERIMENT_ROOT}/functions.sh
 
-rm -f /tmp/share-data/*/*
-
 cp ${PARAMS_PATH}/faiss-hnsw.json ${OSB_PARAMS_PATH}
-setup_environment ${TMP_ENV_DIR} ${TMP_ENV_NAME} ${run_id} faiss-hnsw.json ${test_procedure}
-docker compose --env-file ${ENV_PATH} --env-file ${TMP_ENV_PATH} -f compose.yaml up -d
+setup_environment ${TMP_ENV_DIR} ${TMP_ENV_NAME} "efficient-filtering" faiss-hnsw.json "no-train-test" true
 
-docker_build_command="docker compose --env-file ${ENV_PATH} --env-file ${TMP_ENV_PATH} -f compose.yaml up -d"
+
+docker_build_command="docker compose --env-file ${ENV_PATH} --env-file ${TMP_ENV_PATH} -f compose.yaml build"
+if [[ "$no_cache" == "true" ]]; then
+    docker_build_command+=" --no-cache"
+fi
+
 if [ -n "$service_names" ]; then
   docker_build_command+=" $service_names"
 fi
 
 echo "Building with command $docker_build_command]"
 eval "$docker_build_command"
+
+wait_for_container_stop osb
 
 echo "Finished experiment"
